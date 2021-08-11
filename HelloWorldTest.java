2157da26a1ed5139b0858a62d56aa5a4de77e10a
MEGHNANAGPAL <nagpal.meghna@gmail.com>
2021-08-11 09:46:05 -0400

    Hello World commit
    Hello world

---
+++ HelloWorldTest.java
@@ -0,0 +1,13 @@
+package testProject;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+
+		String print = "hello world. works fine";
+				System.out.println("This is a test. The output is : "+print);
+				
+		
+	}
+
+}
